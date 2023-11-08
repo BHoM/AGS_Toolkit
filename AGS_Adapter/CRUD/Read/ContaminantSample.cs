deaf66352d8addbf910865444c32187056442344
peterjamesnugent <Peter.Nugent@burohappold.com>
2023-11-08 14:12:15 +0000

    Add reading of ContaminantSamples and properties

---
+++ AGS_Adapter/CRUD/Read/ContaminantSample.cs
@@ -0,0 +1,110 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapter;
+using BH.oM.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Ground;
+
+namespace BH.Adapter.AGS
+{
+    public partial class AGSAdapter
+    {
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private List<ContaminantSample> ReadContaminantSamples(List<string> ids = null)
+        {
+            List<string> sectionText = GetSectionText("ERES");
+            string heading = "";
+            int dataIndex = -1;
+
+            // Determine where the section starts
+            for (int i = 0; i < sectionText.Count; i++)
+            {
+                string line = sectionText[i];
+                if (!(line.Length < 5))
+                {
+                    string group = line.Split(',')[0];
+                    if (group.Contains("\"HEADING\""))
+                    {
+                        heading = sectionText[i].Replace("\"", "");
+                    }
+                    else if (group.Contains("\"DATA\""))
+                    {
+                        dataIndex = i;
+                    }
+
+                    if (dataIndex != -1 && heading != "")
+                        break;
+                }
+            }
+
+            if (heading == "")
+            {
+                Engine.Base.Compute.RecordError("The HEADING header is not present in the text file.");
+                return null;
+            }
+            if (dataIndex == -1)
+            {
+                Engine.Base.Compute.RecordError("The DATA header is not present in the text file.");
+                return null;
+            }
+
+            List<string> split = heading.Split(',').ToList();
+
+            Dictionary<string, int> headingIndexes = new Dictionary<string, int>();
+
+            List<string> parameterHeadings = new List<string>()
+            {
+                "LOCA_ID","SAMP_TOP","SAMP_REF","SAMP_TYPE","SAMP_ID","SPEC_REF","SPEC_DPTH","ERES_CODE","ERES_METH","ERES_MATX","ERES_RTYP","ERES_TESN","ERES_NAME",
+                "ERES_TNAM","ERES_RVAL","ERES_RTCD","ERES_RRES","ERES_DETF","ERES_ORG","ERES_RDLM","ERES_MDLM","ERES_QLM",
+                "ERES_DUNI","ERES_TICP","ERES_TICT","ERES_RDAT","ERES_SGRP","SPEC_PREP","SPEC_DESC","ERES_DTIM","ERES_TEST","ERES_TORD","ERES_LOCN","ERES_BAS","ERES_DIL",
+                "ERES_LMTH","ERES_LDTM","ERES_IREF","ERES_SIZE","ERES_PERP","ERES_REM","ERES_LAB","ERES_CRED","TEST_STAT","FILE_FSET"
+            };
+
+            foreach (string parameterHeading in parameterHeadings)
+                headingIndexes.Add(parameterHeading, GetHeadingIndex(parameterHeading, split));
+
+            List<ContaminantSample> contaminantSamples = new List<ContaminantSample>();
+
+            for (int i = dataIndex; i < sectionText.Count; i++)
+            {
+                ContaminantSample contaminantSample = Convert.FromContaminantSample(sectionText[i], headingIndexes);
+                if (contaminantSample != null)
+                    contaminantSamples.Add(contaminantSample);
+            }
+
+            return contaminantSamples;
+        }
+
+        /***************************************************/
+
+    }
+}
+
---
+++ AGS_Adapter/Convert/FromAGS/ContaminantSample.cs
@@ -0,0 +1,144 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapters.AGS;
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.Engine.Geometry;
+using BH.oM.Geometry;
+using BH.oM.Ground;
+
+namespace BH.Adapter.AGS
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static ContaminantSample FromContaminantSample(string text, Dictionary<string, int> headings)
+        { 
+            string id = GetValue(text, "LOCA_ID", headings);
+            string top = GetValue(text, "SAMP_TOP", headings);
+
+            if (top == "")
+                top = GetValue(text, "SPEC_DPTH", headings);
+
+            if (top == "")
+            {
+                if (id == "")
+                    Engine.Base.Compute.RecordWarning($"The top (SAMP_TOP/SPEC_DPTH) value for the contaminant sample is not valid as well as the id (LOCA_ID) and has been skipped.");
+                else
+                    Engine.Base.Compute.RecordWarning($"The top (SAMP_TOP/SPEC_DPTH) value for {id} is invalid and has been skipped.");
+                return null;
+            }
+
+            // ContaminantSample
+            string chemical = GetValue(text, "ERES_CODE", headings);
+            string name = GetValue(text, "ERES_NAME", headings);
+            string result = GetValue(text, "ERES_RVAL", headings);
+            string type = GetValue(text, "SAMP_TYPE", headings);
+
+            List<IContaminantProperty> contaminantProperties = new List<IContaminantProperty>();
+
+            // Contaminant Reference
+            string reference = GetValue(text, "SAMP_REF", headings);
+            string specId = GetValue(text, "SAMP_ID", headings);
+            string receiptDate = GetValue(text, "ERES_RDAT", headings);
+            string batchCode = GetValue(text, "ERES_SGRP", headings);
+            string files = GetValue(text, "FILE_FSET", headings);
+
+            ContaminantReference contaminantReference = Engine.Ground.Create.ContaminantReference(reference, specId, DateTime.Parse(receiptDate), batchCode, files);
+            if (contaminantReference != null)
+                contaminantProperties.Add(contaminantReference);
+
+            // Test Properties
+            string testName = GetValue(text, "ERES_TEST", headings);
+            string labTestName = GetValue(text, "ERES_TNAM", headings);
+            string testReference = GetValue(text, "ERES_TESN", headings);
+            string runType = GetValue(text, "ERES_RTYP", headings);
+            string matrix = GetValue(text, "ERES_MATX", headings);
+            string method = GetValue(text, "ERES_METH", headings);
+            string analysisDate = GetValue(text, "ERES_DTIM", headings);
+            string description = GetValue(text, "SPEC_DESC", headings);
+            string remarks = GetValue(text, "ERES_REM", headings);
+            string testStatus = GetValue(text, "ERES_STAT", headings);
+
+            TestProperties testProperties = Engine.Ground.Create.TestProperties(testName, labTestName, reference,runType, matrix, method, DateTime.Parse(analysisDate), description,
+                remarks, testStatus);
+            if (testProperties != null)
+                contaminantProperties.Add(testProperties);
+
+            // Anaysis Properties
+            string totalOrDissolved = GetValue(text, "ERES_TORD", headings);
+            string accreditingBody= GetValue(text, "ERES_CRED", headings);
+            string labName = GetValue(text, "ERES_LAB", headings);
+            string percentageRemoved = GetValue(text, "ERES_PERP", headings);
+            string sizeRemoved = GetValue(text, "ERES_SIZE", headings);
+            string instrumentReference = GetValue(text, "ERES_IREF", headings);
+            string leachateDate = GetValue(text, "ERES_LDTM", headings);
+            string leachateMethod = GetValue(text, "ERES_LMTH", headings);
+            string diluationFactor = GetValue(text, "ERES_DIL", headings);
+            string basis = GetValue(text, "ERES_BAS", headings);
+            string location = GetValue(text, "ERES_LOCN", headings);
+
+            AnalysisProperties analysisProperties = Engine.Ground.Create.AnalysisProperties(totalOrDissolved, accreditingBody, labName, double.Parse(percentageRemoved), double.Parse(sizeRemoved), instrumentReference, DateTime.Parse(leachateDate),
+                leachateMethod, int.Parse(diluationFactor), basis, location);
+            if (analysisProperties != null)
+                contaminantProperties.Add(analysisProperties);
+
+            // Result Properties
+            string resultType = GetValue(text, "ERES_RTCD", headings);
+            string reportable = GetValue(text, "ERES_RRES", headings);
+            string detectFlag = GetValue(text, "ERES_DETF", headings);
+            string organic = GetValue(text, "ERES_ORG", headings);
+
+            ResultProperties resultProperties = Engine.Ground.Create.ResultProperties(resultType, ParseYNString(reportable), detectFlag, ParseYNString(organic));
+            if (resultProperties != null)
+                contaminantProperties.Add(resultProperties);
+
+            // Detection Properties
+            string detectionLimit = GetValue(text, "ERES_RDLM", headings);
+            string methodDetectionLimit = GetValue(text, "ERES_MDLM", headings);
+            string quantificationLimit = GetValue(text, "ERES_QLM", headings);
+            string ticProbability = GetValue(text, "ERES_TICP", headings);
+            string ticRetention = GetValue(text, "ERES_TICT", headings);
+
+            DetectionProperties detectionProperties = Engine.Ground.Create.DetectionProperties(double.Parse(detectionLimit), double.Parse(methodDetectionLimit), double.Parse(quantificationLimit),
+                double.Parse(ticProbability), double.Parse(ticRetention));
+            if (detectionProperties != null)
+                contaminantProperties.Add(detectionProperties);
+
+            ContaminantSample contaminantSample = Engine.Ground.Create.ContaminantSample(id, double.Parse(top), chemical, name, double.Parse(result), type, contaminantProperties);
+
+            return contaminantSample;
+
+        }
+
+        /***************************************************/
+
+    }
+}
+

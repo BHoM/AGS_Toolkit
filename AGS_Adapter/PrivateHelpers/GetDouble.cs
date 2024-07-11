7a2c15d0fced17821c92b066dc66b47292eaf8ac
Peter Nugent <Peter.Nugent@burohappold.com>
2024-07-11 09:59:16 +0100

    Downgrade headings not being available to warnings

--- AGS_Adapter/PrivateHelpers/GetDouble.cs
+++ AGS_Adapter/PrivateHelpers/GetDouble.cs
@@ -40,7 +40,7 @@ namespace BH.Adapter.AGS
         {
             if (!data.ContainsKey(heading))
             {
-                Compute.RecordError($"The heading {heading} was not found within the data.");
+                Compute.RecordWarning($"The heading {heading} was not found within the data.");
                 return double.NaN;
             }
 
--- AGS_Adapter/PrivateHelpers/GetInt.cs
+++ AGS_Adapter/PrivateHelpers/GetInt.cs
@@ -39,7 +39,7 @@ namespace BH.Adapter.AGS
         {
             if (!data.ContainsKey(heading))
             {
-                Compute.RecordError($"The heading {heading} was not found within the data.");
+                Compute.RecordWarning($"The heading {heading} was not found within the data.");
                 return 0;
             }
 

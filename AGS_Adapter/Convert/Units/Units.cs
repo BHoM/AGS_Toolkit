828c59b93d9bc4279b333baa8c731cb2df117a45
Peter Nugent <Peter.Nugent@BuroHappold.com>
2025-02-26 19:16:36 +0000

    Fix missing unit check

--- AGS_Adapter/Convert/Units/Units.cs
+++ AGS_Adapter/Convert/Units/Units.cs
@@ -118,7 +118,7 @@ namespace BH.Adapter.AGS
                 case "phunit":
                 case "phunits":
                 case "no":
-                case "no units":
+                case "nounits":
                 case "none":
                     return value;
                 default:
